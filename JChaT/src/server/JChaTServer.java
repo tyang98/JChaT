d4dcf41d0f053a36dd9093d766cb533d21cf37d4
Tony <tony.yang98@gmail.com>
2019-12-20 23:10:56 -0500

    implement main

--- JChaT/src/server/JChaTServer.java
+++ JChaT/src/server/JChaTServer.java
@@ -1,5 +1,22 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class JChaTServer {
+
+    final static int portNumber= 3000;
+
+    public static void main(String[] args) {
+        // serverSocket = null;
+
+        try {
+            ServerSocket serverSocket= new ServerSocket(portNumber);
+        } catch (IOException e) {
+            System.err.print("Could not listen on port " + portNumber);
+            System.exit(1);
+
+        }
+    }
 
 }
